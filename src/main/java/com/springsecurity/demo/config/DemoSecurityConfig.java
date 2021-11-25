be7453d95e9785415823d8c5c4d352fe6d684461
akashdeep007 <beingakscool@gmail.com>
2021-11-25 19:36:41 +0530

    Added JDBC Authentication

--- src/main/java/com/springsecurity/demo/config/DemoSecurityConfig.java
+++ src/main/java/com/springsecurity/demo/config/DemoSecurityConfig.java
@@ -1,29 +1,31 @@
 package com.springsecurity.demo.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
+
 
 @Configuration
 @EnableWebSecurity
 public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private DataSource securityDataSource;
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-		UserBuilder users = User.withDefaultPasswordEncoder();
-		auth.inMemoryAuthentication().withUser(users.username("abc").password("abc").roles("USER"));
-		auth.inMemoryAuthentication().withUser(users.username("xyz").password("xyz").roles("USER", "MANAGER"));
-		auth.inMemoryAuthentication().withUser(users.username("admin").password("admim").roles("USER", "ADMIN"));
+		auth.jdbcAuthentication().dataSource(securityDataSource);
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/").hasRole("USER").antMatchers("/leader/**").hasRole("MANAGER")
+		http.authorizeRequests().antMatchers("/").hasRole("EMPLOYEE").antMatchers("/leader/**").hasRole("MANAGER")
 				.antMatchers("/admin/**").hasRole("ADMIN").and().formLogin().loginPage("/myLoginPage")
 				.loginProcessingUrl("/authenticateUser").permitAll().and().logout().permitAll().and()
 				.exceptionHandling().accessDeniedPage("/access-denied");
